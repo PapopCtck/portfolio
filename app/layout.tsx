44a51dab4dbe19520811a5200b6fdfbe49b10e36
PapopCTCK <papop.cha@blockfint.com>
2025-05-01 19:21:01 +0700

    ✨ Finished Hero section (without any animation)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { DM_Sans, Roboto_Mono } from "next/font/google";
+import { DM_Sans, Roboto_Mono, Zeyada } from "next/font/google";
 import "./globals.css";
 import { Dock } from "@ppaop/features/Dock";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
-  subsets: ["latin"],
 });
 
 const robotoMono = Roboto_Mono({
   variable: "--font-roboto-mono",
-  subsets: ["latin"],
+});
+
+const zeyada = Zeyada({
+  variable: "--font-zeyada",
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${dmSans.variable} ${robotoMono.variable} bg-background text-foreground antialiased`}
+        className={`${dmSans.variable} ${robotoMono.variable} ${zeyada.variable} ${dmSans.className} bg-background text-foreground antialiased`}
       >
         {children}
         <Dock />
