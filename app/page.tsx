919731c8363dca0f877ea8cda9058a03688969d6
PapopCTCK <papop.cha@blockfint.com>
2025-06-21 22:35:54 +0700

    🚧 Replaced unfinish section with soon section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import { About } from "@ppaop/features/About";
-import { Footer } from "@ppaop/features/Footer";
-import { GetInTouch } from "@ppaop/features/GetInTouch";
+// import { Footer } from "@ppaop/features/Footer";
+// import { GetInTouch } from "@ppaop/features/GetInTouch";
 import { Hero } from "@ppaop/features/Hero";
 import { PreviousWork } from "@ppaop/features/PreviousWork";
+import { Soon } from "@ppaop/features/Soon";
 
 export default function Home() {
   return (
@@ -10,8 +11,9 @@ export default function Home() {
       <Hero />
       <About />
       <PreviousWork />
-      <GetInTouch />
-      <Footer />
+      {/* <GetInTouch />
+      <Footer /> */}
+      <Soon />
     </div>
   );
 }
